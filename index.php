c1b0fbbed4c3b0f67708b0d190b8484fec8d1907
Xedoi77 <pjsd77@gmail.com>
2020-01-01 17:26:55 +0000

    update

--- index.php
+++ index.php
@@ -4,8 +4,26 @@
 
   $user = new Usuario();
 
-  $user->loadById("3");
+  $user->loadById(3);
 
-  echo $user;
+var_dump($user);
+
+//   echo $user;
+//   echo "<br>";
+
+//   $lista = new Usuario();
+//   $result = $lista->getList();
+//   echo json_encode($result);
+
+    // $search = Usuario::search("Pa");
+    // echo json_encode($search);
+
+    // $usuario = new Usuario();
+    // $usuario->login("Patricia", "789");
+
+    // echo $usuario;
+
+
+
 
 ?>
